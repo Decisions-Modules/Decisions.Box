e2016892315cc257dd71e3278ccd1d8d0430ef13
jeff-decisions <jeff.bordeaux@decisions.com>
2021-03-30 12:59:01 -0400

    Initial Box Module commit

---
+++ Decisions.Box/FolderItemResult.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace Decisions.Box
+{
+    [DataContract]
+    public class FolderItemResult
+    {
+        [DataMember]
+        public int total_count { get; set; }
+
+        [DataMember]
+        public FolderItem[] entries { get; set; }
+
+        [DataMember]
+        public int offset { get; set; }
+
+        [DataMember]
+        public int limit { get; set; }
+
+        [DataMember]
+        public string error_message { get; set; }
+        
+    }
+}
